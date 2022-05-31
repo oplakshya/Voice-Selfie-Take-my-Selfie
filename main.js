5d6d238463ee9e3128f2dc31a3985b2dc1fa7599
oplakshya <85542256+oplakshya@users.noreply.github.com>
2022-05-31 15:50:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+function start() {
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+recognition.onresult = function run(event) {
+    console.log(event);
+    var Content = event.results[0][0].transcript;
+    document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+    speak();
+}
+
+function Speak() {
+    var synth = window.SpeechSynthesis;
+
+    speak_data = document.getElementById("textbox").Value;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+}
+
+Webcam.set({
+    width:360,
+    height:250,
+    image_format : 'png',
+    png_quality:90
+});
+camera = document.getElementById("camera");
